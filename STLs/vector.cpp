af336043a56178f3ac4867957d584c51fa5104df
mritunjay <mritunjay.kumar@delhivery.com>
2024-04-08 09:17:24 +0530

    "arrays learning done"

---
+++ STLs/vector.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+#include<bits/stdc++.h>
+
+
+using namespace std;
+
+int main(){
+    vector<int> v;
+    v.push_back(2);
+    v.push_back(5);
+    for(auto x : v){
+        cout<<x<<endl;
+    }
+    return 0;
+}
